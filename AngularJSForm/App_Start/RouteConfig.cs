4b7d4fbcaa6c0de395c2091520dafc74ad0fa494
maenginner <mauricio.giraldo@mail.escuelaing.edu.co>
2017-02-16 11:45:19 -0500

    lalala

--- AngularJSForm/App_Start/RouteConfig.cs
+++ AngularJSForm/App_Start/RouteConfig.cs
@@ -16,8 +16,7 @@ namespace AngularJSForm
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "UserRegistration", id = UrlParameter.Optional });
         }
     }
 }
